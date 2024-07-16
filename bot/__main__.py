ac70eadf8a461f7667787a1e5635ca30b5437c33
Zakaria <malasicikiwir@gmail.com>
2024-07-16 15:08:38 +0700

    test

--- bot/__main__.py
+++ bot/__main__.py
@@ -93,7 +93,7 @@ async def sts(c, m):
         await m.delete()
         return
     await m.reply_text(
-        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
+        text=(f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`"),
         quote=True
     )
 
