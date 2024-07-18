6c7a45df6978286b6ec5b86926167d05b32b3859
Zakaria <malasicikiwir@gmail.com>
2024-07-18 11:03:22 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -147,7 +147,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
     else:
         await sendMessage(
             message,
-            f"Use me in group.\nVisit <a href="https://t.me/zxh_stuff">Zxh's channel</a> for join link.\n\n<i>This bot made with ❤️ by @ZxhCarkecor</i>",
+            f"""Use me in group.\nVisit <a href="https://t.me/zxh_stuff">Zxh's channel</a> for join link.\n\n<i>This bot made with ❤️ by @ZxhCarkecor</i>""",
             reply_markup,
         )
 
